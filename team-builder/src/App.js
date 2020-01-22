c727e8ce32603e65a0c6cc2d9e84be4e19cae57b
Tyler Alsop <tylerjalsop@gmail.com>
2020-01-21 18:48:06 -0700

    Moved <MemberForm /> into the header in App.js to utilize the styling in index.css

--- team-builder/src/App.js
+++ team-builder/src/App.js
@@ -7,36 +7,26 @@ function App() {
 
   const [forms, setForms] = useState([]);
 
-  const addMember = memberInfo => {
+  // const addMember = memberInfo => {
 
-    const newMember = {
-      name: memberInfo.name,
-      email: memberInfo.email,
-      role: memberInfo.role
-    }
-  }
+  //   const newMember = {
+  //     name: memberInfo.name,
+  //     email: memberInfo.email,
+  //     role: memberInfo.role
+  //   }
+  // }
 
-  const teamRoster = [...forms, newMember];
+  // const teamRoster = [...forms, newMember];
 
-  setForms(teamRoster);
+  // setForms(teamRoster);
 
 
   return (
     <div className="App">
       <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <MemberForm />
       </header>
-      <MemberForm />
+      
     </div>
   )
 };
