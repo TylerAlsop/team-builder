a118f7fe42757e16cbb3443e0320251e66f4dd4f
Tyler Alsop <tylerjalsop@gmail.com>
2020-01-21 18:04:10 -0700

    Worked on passing a setter method in App.js. Need to do more markup in MemberForm.js

--- team-builder/src/App.js
+++ team-builder/src/App.js
@@ -1,10 +1,26 @@
 import React, { useState } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 import MemberForm from "./Components/MemberForm"
 
 function App() {
+
+  const [forms, setForms] = useState([]);
+
+  const addMember = memberInfo => {
+
+    const newMember = {
+      name: memberInfo.name,
+      email: memberInfo.email,
+      role: memberInfo.role
+    }
+  }
+
+  const teamRoster = [...forms, newMember];
+
+  setForms(teamRoster);
+
+
   return (
     <div className="App">
       <header className="App-header">
